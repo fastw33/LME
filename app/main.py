825a5f23f1c4dedddb162a805d756294d7d56d9f
Ebernal <Ed2902@users.noreply.github.com>
2026-08-19 11:49:10 -0500

    Configurar CORS por variables de entorno

--- app/main.py
+++ app/main.py
@@ -33,10 +33,10 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=settings.cors_origins,
+    allow_credentials=settings.cors_allow_credentials,
+    allow_methods=settings.cors_allow_methods,
+    allow_headers=settings.cors_allow_headers,
 )
 
 app.middleware("http")(auth_http_middleware)
